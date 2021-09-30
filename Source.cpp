55e1bfd35e62bb73d7fa2f0fed06266802ad44d8
pelmenprogramist <91686016+pelmenprogramist@users.noreply.github.com>
2021-09-30 18:50:46 +0300

    dz2 integer 2-5

---
+++ Source.cpp
@@ -0,0 +1,83 @@
+//подключение библиотеки
+#include <iostream>
+using namespace std;
+int main(){                                                                                                                                               
+/*begin 25 
+int y;
+int x;
+double x1;
+double x2;
+cout << "input x";
+cin >> x ;
+y =((x*x*x*x*x*x)*3) - (6*(x*x)) - 7;
+cout << "y = "<< y << endl;
+*/
+/*begin26
+int y;
+int x;
+cout << "input x";
+cin >> x;
+y = (4 * (x - 3) * (x - 3) * (x - 3) * (x - 3) * (x - 3) * (x - 3)) - (7 * (x - 3) * (x - 3) * (x - 3)) + 2;
+cout << "y = " << y << endl;
+*/
+/*begin27
+int A;
+double A1;
+double A2;
+double A3;
+cout << "input A";
+cin >> A;
+A1 = A * A;
+A2 = A * A * A * A;
+A3 = A * A * A * A * A * A * A * A;
+cout << "A1 =" << A1 << endl;
+cout << "A2 =" << A2 << endl;
+cout << "A3 =" << A3 << endl;
+*/
+/*begin28
+	int A;
+	double A1;
+	double A2;
+	double A3;
+	double A4;
+	double A5;
+	cout << "input A";
+	cin >> A;
+	A1 = A * A;
+	A2 = A * A * A;
+	A3 = A * A * A * A * A;
+	A4 = A * A * A * A * A * A * A * A * A * A;
+	A5 = A * A * A * A * A * A * A * A * A * A * A * A * A * A * A;
+	cout << "A1 =" << A1 << endl;
+	cout << "A2 =" << A2 << endl;
+	cout << "A3 =" << A3 << endl;
+	cout << "A4 =" << A4 << endl;
+	cout << "A5 =" << A5 << endl;
+	*/
+/*integer 1
+	int L;
+	int M;
+	cout << "input L" ;
+	cin >> L ;
+	M =L / 100;
+	cout << " M v L =" << M << endl;
+*/
+/*integer 2
+	int Kg;
+	int T;
+	cout << "input Kg";
+	cin >> Kg;
+	T = Kg / 1000;
+	cout << " T v Kg=" << T << endl;
+*/
+//integer 3
+	int k;
+	int b;
+	cout << "input k";
+	cin >> k;
+	b = k / 1024;
+	cout << " b v k =" << b << endl;
+}
+
+
+
